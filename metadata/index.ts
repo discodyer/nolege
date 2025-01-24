efa2173a90c2e60c68e5bd5f3ff622685fe25a48
discodyer <cody23333@gmail.com>
2025-01-24 11:34:17 +0800

    update: creator name

--- metadata/index.ts
+++ metadata/index.ts
@@ -22,30 +22,17 @@ export const targetDomain = `https://${plainTargetDomain}`
 /** 创作者 */
 export const creators: Creator[] = [
   {
-    name: '絢香猫',
+    name: 'Cody Gu',
     avatar: '',
-    username: 'nekomeowww',
-    title: 'Nólëbase 原始创作者',
-    desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
+    username: 'discodyer',
+    title: '内容创作者',
+    desc: '电子发烧友',
     links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
+      { type: 'github', icon: 'github', link: 'https://github.com/discodyer' },
+      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/cody2333' },
     ],
-    nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
-    emailAliases: ['neko@ayaka.moe'],
-  },
-  {
-    name: '絢香音',
-    avatar: '',
-    username: 'LittleSound',
-    title: 'Nólëbase 原始创作者',
-    desc: '开源开发者，专注于前端，以及前端相关工具库和工具链开发',
-    links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/LittleSound' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/OikawaRizumu' },
-    ],
-    nameAliases: ['LittleSound', '绚香音', '絢香音', 'Rizumu Oikawa', 'Rizumu Ayaka', 'Ayaka Rizumu', 'Rizumu'],
-    emailAliases: ['rizumu@ayaka.moe', 'rizumu@oqo.moe'],
+    nameAliases: ['discodyer', 'Cody Gu'],
+    emailAliases: ['cody@cd.al'],
   },
 ].map<Creator>((c) => {
   c.avatar = c.avatar || getAvatarUrlByGithubName(c.username)
